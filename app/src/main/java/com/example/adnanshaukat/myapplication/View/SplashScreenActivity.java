99ff370b8a76c60b54ebdabf990dc1d62a9b5c43
Adnan <adnanshaukat425@gmail.com>
2018-12-09 11:55:11 +0500

    All Driver list with respect to transporter in tab layout and recycler view.

---
+++ app/src/main/java/com/example/adnanshaukat/myapplication/View/SplashScreenActivity.java
@@ -0,0 +1,61 @@
+package com.example.adnanshaukat.myapplication.View;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.SyncStateContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.example.adnanshaukat.myapplication.R;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashScreenActivity extends AwesomeSplash {
+
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        getSupportActionBar().hide();
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.fillColor);//any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.logo); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+//        configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
+//        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+//        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+//        configSplash.setAnimPathStrokeDrawingDuration(3000);
+//        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+//        configSplash.setPathSplashStrokeColor(R.color.accent); //any color you want form colors.xml
+//        configSplash.setAnimPathFillingDuration(3000);
+//        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash("");
+        configSplash.setTitleTextColor(R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+    }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
