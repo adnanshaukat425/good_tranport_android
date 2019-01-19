80530ffd7dda8e439d67c4c4b770b67e85cf9b30
Adnan <adnanshaukat425@gmail.com>
2019-01-19 20:26:51 +0500

    After order drivers list shown to customer. Driver drawer added, driver current location added in database, current location of driver shown on map

--- app/src/main/java/com/example/adnanshaukat/myapplication/View/FragmentMainDriver.java
+++ app/src/main/java/com/example/adnanshaukat/myapplication/View/FragmentMainDriver.java
@@ -17,7 +17,7 @@ public class FragmentMainDriver extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_customer, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_driver, container, false);
         return view;
     }
 }
